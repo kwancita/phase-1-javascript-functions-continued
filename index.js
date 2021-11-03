ce6e06db6006397907c6fac6d6270a6153dde146
Wasanta <kwan_u_day@hotmail.com>
2021-11-02 20:41:46 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun()
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(mrk1 = "*",){
+    return function(spc = "special"){ 
+        return `You are ${mrk1}${spc}${mrk1}!`
+    }
+}
